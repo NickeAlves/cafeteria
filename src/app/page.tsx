f60ed0aba6b447ce47e3ccc9ce75bee96bc09e9f
Nicolas Alves <nicke.rodrigues1125@gmail.com>
2025-04-10 12:18:43 +0200

    :sparkles: init project
    Signed-off-by: Nicolas Alves <nicke.rodrigues1125@gmail.com>

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <>
+      <main className="">
+        <div className="flex justify-center p-2">
+          <h1 className="font-bold text-2xl">Cafetería</h1>
+        </div>
+      </main>
+      <footer className=""></footer>
+    </>
+  );
+}
